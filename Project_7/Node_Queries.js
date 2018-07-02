3c5efeabfded22d962e837175629d3f66f1b90bb
rubyverma22 <40520729+rubyverma22@users.noreply.github.com>
2018-07-02 04:22:25 -0700

    Node js Queries

---
+++ Project_7/Node_Queries.js
@@ -0,0 +1,127 @@
+//1.Search all
+
+var mongojs=require('mongojs');
+var db = mongojs('Pro',['deb'])
+db.deb.Search(function(err,res){
+console.log(res);});
+
+//2. Search all by SOUTHEAST-USing projections
+
+var mongojs=require('mongojs');
+var db = mongojs('Pro',['deb'])
+db.deb.Search({},{address:1,city:1,ZONAL REGION:1,name:1,Rating:1,_id:0,postalCode:1,websites:1},function(err,res){
+console.log(res);});
+
+//3.Search by ZONAL REGION
+
+var mongojs=require('mongojs');
+var db = mongojs('Pro',['deb'])
+db.deb.Search({ZONAL REGION:"SOUTHEAST-US"},{address:1,city:1,ZONAL REGION:1,name:1,Rating:1,_id:0,postalCode:1,websites:1},function(err,res){
+console.log(res);});
+
+//4.Search by city
+
+var mongojs=require('mongojs');
+var db = mongojs('Pro',['deb'])
+db.deb.Search({city:"Leland"},{address:1,city:1,ZONAL REGION:1,name:1,Rating:1,_id:0,postalCode:1,websites:1},function(err,res){
+console.log(res);});
+
+//5.Search by name
+
+var mongojs=require('mongojs');
+var db = mongojs('Pro',['deb'])
+db.deb.Search({name:"McDonald's"},{address:1,city:1,ZONAL REGION:1,name:1,Rating:1,_id:0,postalCode:1,websites:1},function(err,res){
+console.log(res);});
+
+//6.Search by ZONAL REGION and city
+
+var mongojs=require('mongojs');
+var db = mongojs('Pro',['deb'])
+db.deb.Search({city:"Leland",ZONAL REGION:"SOUTHEAST-US"},{address:1,city:1,ZONAL REGION:1,name:1,Rating:1,_id:0,postalCode:1,websites:1},function(err,res){
+console.log(res);});
+
+//7.Search by ZONAL REGION and name
+
+var mongojs=require('mongojs');
+var db = mongojs('Pro',['deb'])
+db.deb.Search({ZONAL REGION:"SOUTHEAST-US",name:"McDonald's"},{address:1,city:1,ZONAL REGION:1,name:1,Rating:1,_id:0,postalCode:1,websites:1},function(err,res){
+console.log(res);});
+
+//8.Search by city and name
+
+var mongojs=require('mongojs');
+var db = mongojs('Pro',['deb'])
+db.deb.Search({name:"McDonald's",city:"Leland"},{address:1,city:1,ZONAL REGION:1,name:1,Rating:1,_id:0,postalCode:1,websites:1},function(err,res){
+console.log(res);});
+
+//9.Search by ZONAL REGION,city and name
+
+var mongojs=require('mongojs');
+var db = mongojs('Pro',['deb'])
+db.deb.Search({ZONAL REGION:"SOUTHEAST-US",city:"Leland",name:"McDonald's"},{address:1,city:1,ZONAL REGION:1,name:1,Rating:1,_id:0,postalCode:1,websites:1},function(err,res){
+console.log(res);});
+
+//10.Search all by SOUTHEAST-USing projections
+//and sort by ratings
+
+var mongojs=require('mongojs');
+var db = mongojs('Pro',['deb'])
+db.deb.aggregate([{$match:{}},{$project:{address:1,city:1,ZONAL REGION:1,name:1,Rating:1,_id:0,postalCode:1,websites:1}},{$sort:{Rating:-1}}],function(err,res){
+console.log(res);});
+
+//11.Search by ZONAL REGION
+//and sort by ratings
+
+var mongojs=require('mongojs');
+var db = mongojs('Pro',['deb'])
+db.deb.aggregate([{$match:{ZONAL REGION:"SOUTHEAST-US"}},{$project:{address:1,city:1,ZONAL REGION:1,name:1,Rating:1,_id:0,postalCode:1,websites:1}},{$sort:{Rating:-1}}],function(err,res){
+console.log(res);});
+
+//12.Search by city
+//and sort by ratings
+
+var mongojs=require('mongojs');
+var db = mongojs('Pro',['deb'])
+db.deb.aggregate([{$match:{city:"Leland"}},{$project:{address:1,city:1,ZONAL REGION:1,name:1,Rating:1,_id:0,postalCode:1,websites:1}},{$sort:{Rating:-1}}],function(err,res){
+console.log(res);});
+
+//13.Search by name
+//and sort by ratings
+
+var mongojs=require('mongojs');
+var db = mongojs('Pro',['deb'])
+db.deb.aggregate([{$match:{name:"McDonald's"}},{$project:{address:1,city:1,ZONAL REGION:1,name:1,Rating:1,_id:0,postalCode:1,websites:1}},{$sort:{Rating:-1}}],function(err,res){
+console.log(res);});
+
+//14.Search by ZONAL REGION and city
+//and sort by ratings
+
+var mongojs=require('mongojs');
+var db = mongojs('Pro',['deb'])
+db.deb.aggregate([{$match:{city:"Leland",ZONAL REGION:"SOUTHEAST-US"}},{$project:{address:1,city:1,ZONAL REGION:1,name:1,Rating:1,_id:0,postalCode:1,websites:1}},{$sort:{Rating:-1}}],function(err,res){
+console.log(res);});
+
+
+//15.Search by ZONAL REGION and name
+//and sort by ratings
+
+var mongojs=require('mongojs');
+var db = mongojs('Pro',['deb'])
+db.deb.aggregate([{$match:{ZONAL REGION:"SOUTHEAST-US",name:"McDonald's"}},{$project:{address:1,city:1,ZONAL REGION:1,name:1,Rating:1,_id:0,postalCode:1,websites:1}},{$sort:{Rating:-1}}],function(err,res){
+console.log(res);});
+
+//16.Search by city and name
+//and sort by ratings
+
+var mongojs=require('mongojs');
+var db = mongojs('Pro',['deb'])
+db.deb.aggregate([{$match:{name:"McDonald's",city:"Leland"}},{$project:{address:1,city:1,ZONAL REGION:1,name:1,Rating:1,_id:0,postalCode:1,websites:1}},{$sort:{Rating:-1}}],function(err,res){
+console.log(res);});
+
+//17.Search by ZONAL REGION,city 
+//and name and sort by ratings
+
+var mongojs=require('mongojs');
+var db = mongojs('Pro',['deb'])
+db.deb.aggregate([{$match:{ZONAL REGION:"SOUTHEAST-US",city:"Leland",name:"McDonald's"}},{$project:{address:1,city:1,ZONAL REGION:1,name:1,Rating:1,_id:0,postalCode:1,websites:1}},{$sort:{Rating:-1}}],function(err,res){
+console.log(res);});
