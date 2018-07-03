6134b803c468bd245ff4d27e2eb4309b648444b0
sidrachoudhary <40520746+sidrachoudhary@users.noreply.github.com>
2018-07-02 21:00:19 -0700

    Delete 4.js

--- Project_16/Node.js/4.js
+++
@@ -1,5 +0,0 @@
-//2. To display all hotels in city
-var mongojs=require('mongojs');
-var db = mongojs('pro',['trip'])
- db.trip.aggregate([{$match:{$and:[{city:"Udaipur"}]}},{$project:{area:1,property_name:1,_id:0}}],function(err,res){
-console.log(res)})
