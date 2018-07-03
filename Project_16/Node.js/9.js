bd46097bd4246d77a9f96da439efa4b2415074dc
sidrachoudhary <40520746+sidrachoudhary@users.noreply.github.com>
2018-07-02 21:00:56 -0700

    Delete 9.js

--- Project_16/Node.js/9.js
+++
@@ -1,5 +0,0 @@
-//9. To display hotel name according to city, area and rating
-var mongojs=require('mongojs');
-var db = mongojs('pro',['trip'])
- db.trip.aggregate([{$match:{$and:[{city:"Udaipur"},{hotel_star_rating:"1 star"},{area:"Udaipole"}]}},{$project:{room_types:1,_id:0,property_name:1}}],function(err,res){
-console.log(res)})
