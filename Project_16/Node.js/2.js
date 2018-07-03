9770b51bf48ca85079ecb95db32cdccfab017d1d
sidrachoudhary <40520746+sidrachoudhary@users.noreply.github.com>
2018-07-02 21:00:11 -0700

    Delete 2.js

--- Project_16/Node.js/2.js
+++
@@ -1,5 +0,0 @@
-//2. To search by hotel name 
- var mongojs=require('mongojs');
- var db = mongojs('pro',['trip'])
- db.trip.aggregate([{$match:{property_name:"Luxor Park"}},{$project:{_id:0}}],function(err,res){
- console.log(res)})
