a7a4f85de483d2ca64eced343f1a1ef40e4130d2
sidrachoudhary <40520746+sidrachoudhary@users.noreply.github.com>
2018-07-02 21:00:36 -0700

    Delete 6.js

--- Project_16/Node.js/6.js
+++
@@ -1,5 +0,0 @@
-//6. To display room types and rating of a given hotel
-var mongojs=require('mongojs');
-var db = mongojs('pro',['trip'])
- db.trip.aggregate([{$match:{property_name:"Luxor Park"}},{$project:{room_types:1,_id:0,hotel_star_rating:1}}],function(err,res){
-console.log(res)})
