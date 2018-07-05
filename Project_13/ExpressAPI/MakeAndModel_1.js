a76d180b2365e8eae77f046d8a2621c603765cae
Prateek Jain <jainrajajainprateek@yahoo.com>
2018-07-05 04:53:33 -0700

    Add files via upload

---
+++ Project_13/ExpressAPI/MakeAndModel_1.js
@@ -0,0 +1,23 @@
+var express=require('express');
+var app=express();
+var bodyParser=require('body-parser');
+app.use(bodyParser.json());
+var mongojs=require('mongojs');
+var db=mongojs('PRO',['bike']);
+app.use(express.static(__dirname));
+app.get('/', function(req,res){
+    var make=req.query.make.split(",");
+    var model=req.query.model.split(",");
+    db.bike.aggregate([
+    			{$match:{
+    				Make: {$in:make},
+    				Model:{$in:model}
+    			}},
+    			{$project:{_id:1,Make:1,Price:1,Model:1,Feedback_Perc:1}}
+    	],
+        function(err,docs){
+        res.json(docs);
+    });
+});
+app.listen(3000);
+console.log("Server running");
---
+++ Project_13/ExpressAPI/Price_3.js
@@ -0,0 +1,24 @@
+var express=require('express');
+var app=express();
+var bodyParser=require('body-parser');
+app.use(bodyParser.json());
+var mongojs=require('mongojs');
+var db=mongojs('PRO',['bike']);
+app.use(express.static(__dirname));
+app.get('/:Pricelow/:Pricemax', function(req,res){
+    db.bike.aggregate([
+          {$match:
+            {$and:[
+              {Price: {$gte:parseInt(req.params.Pricelow)}},
+              {Price: {$lte:parseInt(req.params.Pricemax)}}
+            ]}
+          },
+        {$project:{_id:1,Make:1,Price:1,Model:1,Feedback_Perc:1}},
+        {$sort:{Feedback_Perc:1}}
+      ],
+        function(err,docs){
+        res.json(docs);
+    });
+});
+app.listen(3000);
+console.log("Server running");
