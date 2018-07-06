9014601734eeb8318c17bebd4cda496d195fd077
shashwatacharya <40520735+shashwatacharya@users.noreply.github.com>
2018-07-06 07:24:26 -0700

    Delete address.js

--- Project_4/NodeJs/address.js
+++
@@ -1,6 +0,0 @@
-/* Search on the basis of Address*/
-var mongojs = require('mongojs');
-var db = mongojs('CLEAR',['cleartrip']);
-db.cleartrip.find({address:"High Court Colony, Jodhpur. Rajasthan (India)."}),function(err,res){
-console.log(res);
-});
