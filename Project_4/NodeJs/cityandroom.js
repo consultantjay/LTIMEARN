b2800586a40f85fb93b9e6dddb1c7e102e03fb9f
shashwatacharya <40520735+shashwatacharya@users.noreply.github.com>
2018-07-06 07:25:43 -0700

    Delete cityandroom.js

--- Project_4/NodeJs/cityandroom.js
+++
@@ -1,7 +0,0 @@
-/* search on the basis of city and room count*/
-var mongojs = require('mongojs');
-var db = mongojs('Hotel',['cleartrip']);
-db.cleartrip.find({$and:[{city:'Jodhpur'},{room_count:{$gte:2}}]},{property_name:1,rating:1,room_facilities:1,_id:0},function(err,res){
-console.log(res);
-}
-)
