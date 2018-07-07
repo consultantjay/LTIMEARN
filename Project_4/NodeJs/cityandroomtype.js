54b12be6321d990a2b8b60bbdcdd65572f1aa81c
shashwatacharya <40520735+shashwatacharya@users.noreply.github.com>
2018-07-06 23:27:23 -0700

    Update cityandroomtype.js

--- Project_4/NodeJs/cityandroomtype.js
+++ Project_4/NodeJs/cityandroomtype.js
@@ -2,7 +2,7 @@
 var mongojs = require('mongojs');
 var db = mongojs('Hotel',['cleartrip']);
 db.cleartrip.aggregate([{$match:{$and:[{city:/Jodhpur/i},
-{room_type:/Standard/i}]}},
+{room_type:/Double Room/i}]}},
 {$project:{_id:1,property_name:1}}],
 function(err,res){
 		console.log(res);
