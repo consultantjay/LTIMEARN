80d731a6f3a28b7c3006649d537db16431e40d0d
shashwatacharya <40520735+shashwatacharya@users.noreply.github.com>
2018-07-06 23:29:22 -0700

    Update roomtype.js

--- Project_4/NodeJs/roomtype.js
+++ Project_4/NodeJs/roomtype.js
@@ -2,7 +2,7 @@
 var mongojs = require('mongojs');
 var db = mongojs('Hotel',['cleartrip']);
 db.cleartrip.aggregate([{$match:{$and:[
-{room_type:/Standard/i}]}},
+{room_type:/Double Room/i}]}},
 {$project:{_id:1,property_name:1}}],
 function(err,res){
 		console.log(res);
