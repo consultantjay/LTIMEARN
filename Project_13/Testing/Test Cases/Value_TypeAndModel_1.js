23600f369cd622120d5ef539635bbc76eb206b70
Prateek Jain <jainrajajainprateek@yahoo.com>
2018-07-07 21:13:11 -0700

    Add files via upload

---
+++ Project_13/Testing/Test Cases/Value_TypeAndModel_1.js
@@ -0,0 +1,15 @@
+var expect = require('chai').expect;
+var mongojs = require('mongojs');
+var db = mongojs('PRO',['bike']);
+var value;
+describe('Value Test By Type Model',function(){
+    before(function (done){
+		db.bike.find({Type:"Standard",Model:"R-Series"}, function (err,res){ 
+	        value=res[0].Model_Year;
+			done();
+		});
+	});
+	it('Value of Model Year',function(){
+        expect(value).to.equal(1970);
+    });
+});
