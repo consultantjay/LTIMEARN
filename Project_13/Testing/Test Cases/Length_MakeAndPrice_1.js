f2e88e8a8f10916f71a8f08b735f3b5ac9e0a004
Prateek Jain <jainrajajainprateek@yahoo.com>
2018-07-07 21:10:23 -0700

    Add files via upload

---
+++ Project_13/Testing/Test Cases/Length_MakeAndPrice_1.js
@@ -0,0 +1,18 @@
+var expect = require('chai').expect;
+var mongojs = require('mongojs');
+var db = mongojs('PRO',['bike']);
+var val_count=0;
+describe('Test by Make Price length',function(){
+	before(function(done){
+  		db.bike.find(
+			{Make:"BMW",Price:14640},
+			{_id:1,Make:1,Price:1,Model:1,Feedback_Perc:1},
+			function(err,res){
+				val_count = Object.keys(res[0]).length;
+				done();
+			});
+	});
+	it('_id,Make,Price,Model,Feedback_Perc in Projection',function(){
+		expect(val_count).to.equal(5);
+	});
+});
