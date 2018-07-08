6bf99bf20f0c7ba275f970e626f04ad61c401ab6
ST1097 <31993745+ST1097@users.noreply.github.com>
2018-07-09 00:37:31 +0530

    Uploaded test cases

---
+++ Project_11/Testing/test/cityCuisineVisitor_type.js
@@ -0,0 +1,23 @@
+var expect = require('chai').expect;
+var mongojs = require('mongojs');
+var db = mongojs('LTI',['restopedia']);
+
+var type;
+describe('Testing type',function(){
+
+	before(function (done){
+        db.restopedia.aggregate([ 
+            { $match: {City:"Ober-Moerlen",Cuisine:"Scotish", "Good for(suitable)":"Non Vegetarian"} },     
+            { $project: {Name:1, Address:1, Phone:1, City:1, State:1, Country:1, Rating:1}  },            
+            { $sort: {Rating:-1} }],              
+            function(err, res){           
+                type=typeof res[0].Rating;
+				done(); 
+            }     
+         );
+	});
+
+	it('by length',function(){
+		expect(type).to.equal("number");
+	});
+});
