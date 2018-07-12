b67425ed15280ac46f1694b2919386dea776ec42
prachitiwari538 <40520698+prachitiwari538@users.noreply.github.com>
2018-07-11 21:04:11 -0700

    Update All.js

--- Project_14/ExpressService_API/All.js
+++ Project_14/ExpressService_API/All.js
@@ -1,15 +1,25 @@
  var express = require('express');
+ var cors = require('cors');
  var app = express();
  var bodyParser = require('body-parser');
  app.use(bodyParser.json());
  var mongojs = require('mongojs');
  var db = mongojs('Project',['hospital']);
+ var cors = require('cors');
+ app.use(cors());
  app.use(express.static(__dirname));
 
- app.get('/list',function(req,res){
+ app.get('/hospital',function(req,res){
    db.hospital.find(function(err,docs){
      res.json(docs);
    })
+});
+
+
+app.get('/hospital/:hid',function(req,res){
+  db.hospital.find({"Provider_ID":parseInt(req.params.hid)},function(err,docs){
+    res.json(docs);
+  });
 });
 
 app.listen(3000);
